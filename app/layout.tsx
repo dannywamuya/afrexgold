717a99cc48eebf6986681b39f3cdc7c783671eca
dannywamuya <dwamuya@gmail.com>
2024-07-25 12:27:34 +0300

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-	title: 'Afrex Gold Limited',
+	title: 'Afrex Gold',
 	description:
 		'Proudly Kenyan exporters of fresh fruits and vegetables for more than 20 years!',
 };
